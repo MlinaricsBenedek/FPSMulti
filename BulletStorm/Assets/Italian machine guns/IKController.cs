03b1d00cba5ecc70e12469b1fe3b35297797fd20
MlinaricsBenedek <144675299+MlinaricsBenedek@users.noreply.github.com>
2025-03-31 20:36:32 +0200

    CharcaterHoldMG

---
+++ BulletStorm/Assets/Italian machine guns/IKController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKController : MonoBehaviour
+{
+    public Animator animator;
+    public Transform rightHandTarget; // A fegyver fogópontja
+    public Transform leftHandTarget;  // A másik kéz is foghatja
+    //private bool isHoldingGun = false; // Alapértelmezett állapot
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        if (animator)
+        {
+            // Jobb kéz mozgásának beállítása
+            if (rightHandTarget != null)
+            {
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+                animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandTarget.position);
+                animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandTarget.rotation);
+            }
+
+            // Bal kéz mozgásának beállítása (ha támogatott)
+            if (leftHandTarget != null)
+            {
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+                animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+                animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandTarget.position);
+                animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandTarget.rotation);
+            }
+        }
+        //else
+        //{
+        //    // Ha nincs fegyver, engedjük el az IK-t, hogy visszatérjen az alapállapotba
+        //    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+        //    animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
+        //    animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 0);
+        //    animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 0);
+        //}
+    }
+}
